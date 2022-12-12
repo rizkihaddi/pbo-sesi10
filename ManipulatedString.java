437c19f7a0e34e43611233758a972c8817b61f81
Rizki Haddi Prayoga <rizkihaddiprayoga@gmail.com>
2022-12-12 16:22:52 +0700

    upload tugas

---
+++ ManipulatedString.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class ManipulatedString {
+  public static void main(String[] args) {
+    System.out.println("==================================");
+    System.out.print("Inputkan kata: ");
+    Scanner sc = new Scanner(System.in);
+    String inputText = sc.nextLine();
+    System.out
+        .println("Kebalikan dari kata" + " " + inputText + " " + "adalah: " + ManipulatedString.reverse(inputText));
+    ManipulatedString.Pallindrom(inputText, ManipulatedString.reverse(inputText));
+
+    sc.close();
+  }
+
+  private static String reverse(String text) {
+    String s = text;
+    Integer jml = s.length();
+    String hasil = "";
+
+    for (int i = jml; i > 0; i--) {
+      hasil = hasil + s.substring(i - 1, i);
+    }
+
+    return hasil;
+  }
+
+  public static void Pallindrom(String text, String textreverse) {
+    String s = text;
+    String r = textreverse;
+
+    boolean Pallindrom = s.equalsIgnoreCase(r);
+    if (Pallindrom) {
+      System.out.println("Kata" + " " + s + " " + "adalah Palindrom");
+    } else {
+      System.out.println("Kata" + " " + s + " " + "Bukan Pallindrom");
+    }
+    System.out.println("==================================");
+  }
+}
